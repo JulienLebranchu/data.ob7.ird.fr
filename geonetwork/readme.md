4d0a3dd6f52aa1f1f60f812cf39923dd8b56981c
Andrew Conway <a.conway27@gmail.com>
2019-04-16 11:29:26 +0100

    Files added.

---
+++ geonetwork/readme.md
@@ -0,0 +1,110 @@
+# Marine Institute Data Catalogue: GeoNetwork
+
+This repository contains the custom files for the [GeoNetwork](https://geonetwork-opensource.org/) implementation of the
+Marine Institute's Data Cataologue.
+
+---
+
+# How to Build the MI Data Catalogue geonetwork application
+
+The external web portal for the MI Data Catalogue is based on GeoNetwork 3 with MI customizations:
+These include:
+
+- UI Changes to include an MI look & feel.
+- Custom search facets
+- Additional plugins: SensorML, SeaDataNet CDI, SeaDataNet CSR. For more information on building plugins for geonetwork refer to: <https://github.com/metadata101/sensorML>
+
+## Prerequisites
+The following tools are required to be installed to setup a development environment for GeoNetwork:
+
+For details on the latest supported versions, please refer to: GeoNetwork Software Development Guide <https://github.com/geonetwork/core-geonetwork/tree/master/software_development>
+
+- **Java**: Developing with GeoNetwork requires Java Development Kit (JDK) 1.8 or greater.
+- **Maven** 3.1.0+ GeoNetwork uses Maven to manage the build process and the dependencies.
+    Install Maven from following the Maven Install Guide: <https://maven.apache.org/install.html>. &nbsp;
+    Manually set the following: Set the JAVA_HOME path (e.g. C:\Program Files\Java\jdk1.8.0_171​) &nbsp;
+    Add the Maven bin location to your 'path' environment variable. (e.g. C:\Program Files\apache-maven-3.5.4\bin​)
+- **Git**: GeoNetwork source code is stored and versioned in a Git repository on Github. Install Git for Windows. <https://git-scm.com/downloads>
+- **Ant**: GeoNetwork uses Ant to build the installer. Install from Ant Install Guide​: <https://ant.apache.org/manual/install.html> &nbsp;
+    Manually set the Ant bin loaction to you 'path' environment variable. (e.g. C:\Program Files\apache-ant-1.10.4\bin)
+- **Sphinx** - To create the GeoNetwork documentation in a nice format Sphinx is used.
+
+## Cloning the MI Data Catalogue Repository (this repo)
+Ensure you have cloned the MI Data Catalogue Repository (this repo). This ensures you have the latest build scripts and MI specific file changes before starting the build.
+
+Steps to clone MI Data Catalogue repository:
+
+- Launch a new Git CMD Window and naviagte to (or create) a folder for your git local repositories.
+- Run: `git clone --recurse-submodules https://github.com/IrishMarineInstitute/data-catalogue.git data-catalogue`
+
+
+## Detailed Build Steps:
+
+**IMPORTANT: Any files included in this github repository must be included in the 'mi_geonetwork_build' maven config script, otherwise files will not be picked up by the build. Ensure your files are included in the build script before starting a build.**
+
+Run the following:
+- Launch a new Git CMD Window and naviagte to (or create) a folder for your git local repositories.
+
+Run the commands:
+- `git clone --recurse-submodules https://github.com/geonetwork/core-geonetwork.git geonetwork`
+- `cd geonetwork`
+- `git checkout tags/3.4.2 -b build`
+- `git status`
+
+Run a clean build by running the command:
+- `mvn clean install -DskipTests`
+
+The build should be sucessfull.
+Note: Maven default location: C:\Users\<user>\.m2\repository. Output WAR is stored in: \geonetwork\web\target
+
+Pull in sub-modules for Plugins:
+
+- navigate to 'geonetwork\schemas' directory.
+
+Run commands:
+- `git submodule add https://github.com/metadata101/sensorML.git sensorML`
+- `git submodule add https://github.com/metadata101/iso19139.sdn-csr.git`
+- `git submodule add https://github.com/metadata101/iso19139.sdn-cdi.git`
+
+Navigate to the data-catalogue repository and run the MI script: mi_geonetwork_build.xml. This is an ant build script and will copy all required MI content into the build branch of the geonetwork repository (created earlier)
+
+- `cd data-catalogue`
+- `ant -buildfile mi_geonetwork_build.xml`
+
+The build (copy files) should complete successfully.
+
+The final step is to navigate back to the 'geonetwork' repository and re-run the Maven build. This will build a new version of the geonetwork application with MI specific changes applied.
+
+- `mvn clean install -DskipTests`
+
+The build should be sucessfull. The final build output is an output WAR file stored in: \geonetwork\web\target.
+
+## geonetwork h2 Database:
+The default loctaion of the geonetwork h2 databse is: `\Apache Software Foundation\Tomcat 9.0`
+The following files are created:
+
+`gn.h2.db`
+`gn.lock.db`
+`gn.trace.db`
+`gn.trace.db.old`
+
+To deploy a fully clean version, the above files should be deleted and a new data harvest performed.
+
+
+## Deployment
+- Take WAR file made from the steps described here.
+- Deploy in **WebApps** folder of an installed Tomcat instance - Stop and Start Tomcat. (Memory of 2GB needed - Java 8 and Tomcat 8 recommended by devs (9 works too though).
+- Log-in to the GeoNetwork instance (initially default as `Admin/Admin`): `/geonetwork/srv/eng/catalog.signin`
+- Under Admin Console:
+	- Change the password: `/geonetwork/srv/eng/admin.console#/organization`
+	- Change the catalogue name, organisation, host (e.g. [data.marine.ie]), preferred protocol and save changes: `/geonetwork/srv/eng/admin.console#/settings`
+	- Run harvest: Create directory harvest (name it), run from the folder where the XML data has been saved to, 
+		- Group: `Sample Group`
+		- User: `Admin`
+		- Directory: `Local file location`
+		- Uncheck `keep catalog record even if deleted at source`
+		- Type of Record: `Metadata`
+		- Ensure all is clicked
+- Go to Tools and click `rebuild index`.
+
+- **Alternatively**, use the *dockerfile* included in this repo.
---
+++ geonetwork/web-ui/src/main/resources/catalog/components/search/mdview/mdviewModule.js
@@ -0,0 +1,207 @@
+/*
+ * Copyright (C) 2001-2016 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+
+(function () {
+    goog.provide('gn_mdview');
+
+
+
+
+    goog.require('gn_mdview_directive');
+    goog.require('gn_mdview_service');
+    goog.require('gn_related_observer_directive');
+    goog.require('gn_userfeedback');
+    goog.require('gn_thesaurus');
+    goog.require('gn_catalog_service');
+
+    var module = angular.module('gn_mdview', [
+      'gn_mdview_service',
+      'gn_mdview_directive',
+      'gn_related_observer_directive',
+      'gn_userfeedback',
+      'gn_thesaurus',
+      'gn_catalog_service'
+    ]);
+
+    module.controller('GnMdViewController', [
+      '$scope', '$http', '$compile', 'gnSearchSettings', 'gnSearchLocation',
+      'gnMetadataActions', 'gnAlertService', '$translate', '$location',
+      'gnMdView', 'gnMdViewObj', 'gnMdFormatter', 'gnConfig',
+      function ($scope, $http, $compile, gnSearchSettings, gnSearchLocation,
+               gnMetadataActions, gnAlertService, $translate, $location,
+               gnMdView, gnMdViewObj, gnMdFormatter, gnConfig) {
+
+          $scope.formatter = gnSearchSettings.formatter;
+          $scope.gnMetadataActions = gnMetadataActions;
+          $scope.usingFormatter = false;
+          $scope.url = location.href;
+          $scope.compileScope = $scope.$new();
+          $scope.recordIdentifierRequested = gnSearchLocation.getUuid();
+          $scope.isUserFeedbackEnabled = false;
+          $scope.isRatingEnabled = false;
+
+          statusSystemRating =
+             gnConfig[gnConfig.key.isRatingUserFeedbackEnabled];
+          if (statusSystemRating == 'advanced') {
+              $scope.isUserFeedbackEnabled = true;
+          }
+          if (statusSystemRating == 'basic') {
+              $scope.isRatingEnabled = true;
+          }
+
+          $scope.search = function (params) {
+              $location.path('/search');
+              $location.search(params);
+          };
+          $scope.deleteRecord = function (md) {
+              return gnMetadataActions.deleteMd(md).then(function (data) {
+                  gnAlertService.addAlert({
+                      msg: $translate.instant('metadataRemoved',
+                          { title: md.title || md.defaultTitle }),
+                      type: 'success'
+                  });
+                  $scope.closeRecord(md);
+              }, function (reason) {
+                  // Data needs improvements
+                  // See https://github.com/geonetwork/core-geonetwork/issues/723
+                  gnAlertService.addAlert({
+                      msg: reason.data,
+                      type: 'danger'
+                  });
+              });
+          };
+
+          // activate the tabs in the advanded metadata view
+          $scope.activateTabs = function () {
+
+              // attach click to tab
+              $('.nav-tabs-advanced a').click(function (e) {
+                  e.preventDefault();
+                  $(this).tab('show');
+              });
+              // hide empty tab
+              $('.nav-tabs-advanced a').each(function () {
+
+                  var tabLink = $(this).attr('href');
+
+                  if (tabLink) {
+                      if ($(tabLink).length === 0) {
+                          $(this).parent().hide();
+                      }
+                  }
+              });
+              // show the first tab
+              $('.nav-tabs-advanced a:first').tab('show');
+          };
+
+          $scope.format = function (f) {
+              $scope.usingFormatter = f !== undefined;
+              $scope.currentFormatter = f;
+              if (f) {
+                  gnMdFormatter.getFormatterUrl(f.url, $scope).then(function (url) {
+                      $http.get(url, {
+                          headers: {
+                              Accept: 'text/html'
+                          }
+                      }).then(
+                          function (response) {
+                              var snippet = response.data.replace(
+                                  '<?xml version="1.0" encoding="UTF-8"?>', '');
+
+                              $('#gn-metadata-display').find('*').remove();
+
+                              $scope.compileScope.$destroy();
+
+                              // Compile against a new scope
+                              $scope.compileScope = $scope.$new();
+                              var content = $compile(snippet)($scope.compileScope);
+
+                              $('#gn-metadata-display').append(content);
+
+                              // activate the tabs in the full view
+                              $scope.activateTabs();
+                          });
+                  });
+              }
+          };
+
+          // Reset current formatter to open the next record
+          // in default mode.
+          $scope.$watch('mdView.current.record', function () {
+
+              $scope.usingFormatter = false;
+              $scope.currentFormatter = null;
+
+
+              //remove old schema scripts
+              var oldSchema = document.getElementById("schemaDotOrg");
+              if (oldSchema) {
+                  oldSchema.parentNode.removeChild(oldSchema);
+              }
+
+              //Load new schema scripts
+              if (gnMdViewObj.current.record != null) {
+
+                  gnMdViewObj.current.record.identifierVisible = [];
+
+                  for (var i = 0; i < gnMdViewObj.current.record.identifier.length; i++) {
+
+                      var idString = gnMdViewObj.current.record.identifier[i];
+
+                      if (typeof idString !== "string") {
+                          idString = JSON.stringify(idString);
+                      }
+
+                      if (idString.startsWith('{"@context"')) {
+                          gnMdViewObj.current.record.identifierVisible[i] = false;
+
+                          var scriptTag = document.getElementById("schemaDotOrg");
+
+                          if (scriptTag) {
+                              scriptTag.innerHTML = idString; //update existing script tag
+                          }
+                          else {
+                              //create script tag
+                              scriptTag = document.createElement('script');
+                              scriptTag.id = "schemaDotOrg";
+                              scriptTag.type = "application/ld+json";
+                              scriptTag.innerHTML = idString;
+
+                              document.getElementsByTagName('head')[0].appendChild(scriptTag);
+                          }
+                      }
+                      else {
+                          gnMdViewObj.current.record.identifierVisible[i] = true;
+                      }
+                  }
+              }
+
+          });
+
+          // Know from what path we come from
+          $scope.gnMdViewObj = gnMdViewObj;
+          $scope.$watch('gnMdViewObj.from', function (v) {
+              $scope.fromView = v ? v.substring(1) : v;
+          });
+      }]);
+})();
